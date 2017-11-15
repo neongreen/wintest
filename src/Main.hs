8d56a0df04f45f8dc514937c862de9e424a58bb2
Artyom <yom@artyom.me>
2017-11-15 14:36:21 +0300

    use createFileA

--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,16 @@
 module Main where
 
 import CF
+import System.Win32
 
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  withCString "бла" $ \fname ->
+    createFileA
+      fname
+      gENERIC_WRITE
+      fILE_SHARE_NONE
+      (maybePtr Nothing)
+      cREATE_ALWAYS
+      fILE_ATTRIBUTE_NORMAL
+      (maybePtr Nothing)
